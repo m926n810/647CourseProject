34dd71b4084d0e2ca18f3da562cca54463fd20ff
m926n810 <m926n810@ku.edu>
2022-05-04 19:47:00 -0500

    yes

--- randomdatageneratorphpfile/execute.php
+++ randomdatageneratorphpfile/execute.php
@@ -24,13 +24,14 @@
             while($row = $result->fetch_assoc()){
                 $hrsprs = rand(80,800);
                 $weight = rand(2000,5000);
-                $milehigh = rand(14, 60);
-                $milecity = rand(14, 60);
-
+                
                 while($evrow=$EV->fetch_assoc()){
                     if($row['ModelName'] == $evrow['ModelName']){
                         $milehigh = 'n/a';
                         $milecity = 'n/a';
+                    }else{
+                        $milehigh = rand(14, 60);
+                        $milecity = rand(14, 60);
                     }
                 }
                 if(rand(0,1)==0) $blind='yes';
