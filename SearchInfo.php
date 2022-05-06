2011eeeb792b79937bba3783e5b3a7460d98c287
m926n810 <m926n810@ku.edu>
2022-05-06 02:56:13 -0500

    ssssssssssssss

--- SearchInfo.php
+++ SearchInfo.php
@@ -34,8 +34,7 @@ if($brand == '*' || $model == '*'){
     $query = "SELECT MAKES.BrandName,FEATURES.ModelName,FEATURES.ModelTrim,HorsePower,Weightt,Mileleague_high,Mileleague_city,Blindspotcensor,Backupcamera,AWD,GPS,MSRP,BodyType,SafetyRating FROM FEATURES 
                 JOIN MODEL ON MODEL.ModelName=FEATURES.ModelName
                 JOIN MAKES ON MODEL.ModelName=MAKES.ModelName";
-    if($brand!='*') $query=$query." AND MAKES.BrandName=$brand";
-    $query=$query." ORDER BY BRAND.BrandName, MODEL.ModelName;";
+
 
     echo $query;
    
@@ -43,7 +42,6 @@ if($brand == '*' || $model == '*'){
     $queryelse="SELECT MAKES.BrandName,FEATURES.ModelName,FEATURES.ModelTrim,HorsePower,Weightt,Mileleague_high,Mileleague_city,Blindspotcensor,Backupcamera,AWD,GPS,MSRP,BodyType,SafetyRating FROM FEATURES 
                 JOIN MODEL ON MODEL.ModelName=FEATURES.ModelName AND MODEL.ModelName=$model
                 JOIN MAKES ON MODEL.ModelName=MAKES.ModelName AND MAKES.BrandName=$brand";
-    if($trim!='*'){ $queryelse=$queryelse." AND FEATURES.ModelTrim=$trim;";}
     
     echo "<br>".$queryelse;
 
