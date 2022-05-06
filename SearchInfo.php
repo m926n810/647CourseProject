75c7ea15b2218bdbdf095cdfcb18d188f48962b0
m926n810 <m926n810@ku.edu>
2022-05-06 03:19:00 -0500

    sadsdasdsad

--- SearchInfo.php
+++ SearchInfo.php
@@ -31,7 +31,7 @@ if($brand == '*' || $model == '*'){
             <th>Hot/cold seats</th>
             <th>Sunroof<th>
         </tr>";
-    $query = "SELECT MAKES.BrandName,FEATURES.ModelName,FEATURES.ModelTrim,HorsePower,Weightt,Mileleague_high,Mileleague_city,Blindspotcensor,Backupcamera,AWD,GPS,MSRP,BodyType,SafetyRating FROM FEATURES 
+    $query = "SELECT MAKES.BrandName,FEATURES.ModelName,FEATURES.ModelTrim,HorsePower,Weightt,Mileleague_high,Mileleague_city,Blindspotcensor,Backupcamera,AWD,GPS,MSRP,BodyType,SafetyRating,TemperatureSeats,Sunroof FROM FEATURES 
                 JOIN MODEL ON MODEL.ModelName=FEATURES.ModelName
                 JOIN MAKES ON MODEL.ModelName=MAKES.ModelName";
     if($brand!='*') $query=$query." AND MAKES.BrandName='$brand'";
@@ -43,14 +43,14 @@ if($brand == '*' || $model == '*'){
             echo "<tr>
                     <td>".$row['BrandName']."</td>
                     <td>".$row['ModelName']."</td>
-                    <td>".$row['MSRP']."</td>
+                    <td>$".number_format(intval($rowelse["MSRP"]),2,'.',',')."</td>
                     <td>".$row['BodyType']."</td>
-                    <td>".$row['SafetyRating']."</td>
+                    <td>".$row['SafetyRating']."/5</td>
                     <td>".$row['ModelTrim']."</td>
-                    <td>".$row['HorsePower']."</td>
-                    <td>".$row['Weightt']."</td>
-                    <td>".$row['MileLeague_city']."</td>
-                    <td>".$row['MileLeague_high']."</td>
+                    <td>".$row['HorsePower']." hp</td>
+                    <td>".$row['Weightt']." ibs</td>
+                    <td>".$row['MileLeague_city']." mpg</td>
+                    <td>".$row['MileLeague_high']." mpg</td>
                     <td>".$row['Blindspotcensor']."</td>
                     <td>".$row['BackupCamera']."</td>
                     <td>".$row['AWD']."</td>
@@ -63,7 +63,7 @@ if($brand == '*' || $model == '*'){
     echo '</table>';
     $result->free();       
 }else{
-    $queryelse="SELECT MAKES.BrandName,FEATURES.ModelName,FEATURES.ModelTrim,HorsePower,Weightt,Mileleague_high,Mileleague_city,Blindspotcensor,Backupcamera,AWD,GPS,MSRP,BodyType,SafetyRating FROM FEATURES 
+    $queryelse="SELECT MAKES.BrandName,FEATURES.ModelName,FEATURES.ModelTrim,HorsePower,Weightt,Mileleague_high,Mileleague_city,Blindspotcensor,Backupcamera,AWD,GPS,MSRP,BodyType,SafetyRating,TemperatureSeats,Sunroof FROM FEATURES 
                 JOIN MODEL ON MODEL.ModelName=FEATURES.ModelName AND MODEL.ModelName='$model'
                 JOIN MAKES ON MODEL.ModelName=MAKES.ModelName AND MAKES.BrandName='$brand'";
     if($trim!='*'){ $queryelse=$queryelse." AND FEATURES.ModelTrim='$trim';";}
@@ -88,23 +88,23 @@ if($brand == '*' || $model == '*'){
                         </tr>
                         <tr>    
                             <th>Safety rating: </th>
-                            <td>'.$rowelse["SafetyRating"].'</td>
+                            <td>'.$rowelse["SafetyRating"].' out of 5</td>
                         </tr>
                         <tr>    
                             <th>Horsepower: </th>
-                            <td>'.$rowelse["HorsePower"].'</td>
+                            <td>'.$rowelse["HorsePower"].' horsepower</td>
                         </tr>
                         <tr>    
                             <th>Weight: </th>
-                            <td>'.$rowelse["Weightt"].'</td>
+                            <td>'.$rowelse["Weightt"].' pounds</td>
                         </tr>
                         <tr>    
                             <th>Mileleague city: </th>
-                            <td>'.$rowelse["Mileleague_city"].'</td>
+                            <td>'.$rowelse["Mileleague_city"].' miles per gallon</td>
                         </tr>
                         <tr>    
                             <th>Mileleague highway: </th>
-                            <td>'.$rowelse["Mileleague_high"].'</td>
+                            <td>'.$rowelse["Mileleague_high"].' miles per gallons</td>
                         </tr>
                     </table>
                 </div>
